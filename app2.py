71ee70d24d33dd1adceb10b7c4da916a6728254d
Tariro Gwandiwa <95386578+fildahgwen@users.noreply.github.com>
2024-05-13 09:49:39 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -19,7 +19,29 @@ def predict_cluster(text):
     return cluster
 
 # Streamlit app
-st.title('Story Clustering App')
+# Set the background color to Benjamin Moore Hale Navy
+st.markdown(
+    """
+    <style>
+    body {
+        background-color: #4D648D; /* Benjamin Moore Hale Navy */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Add the newspaper icon to the app titlest.title("<i class='fas fa-newspaper'></i> Your App Title", unsafe_allow_html=True)
+
+# Include Font Awesome library
+st.markdown('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">', unsafe_allow_html=True)
+
+# Add the newspaper icon to the app title
+st.title("<i class='fas fa-newspaper'></i> Your App Title", unsafe_allow_html=True)
+##
+
+
+##st.title('Story Clustering App')
 
 text_input = st.text_input('Enter a story:')
 if st.button('Predict Cluster'):
